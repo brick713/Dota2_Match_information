801fa7acebcadf9b32561b35437e17789a63ddb9
brick713 <hibrick713@gmail.com>
2022-08-19 09:22:08 +0800

    update

--- test.py
+++ test.py
@@ -2,5 +2,6 @@ from dota import dota
 
 dota_obj = dota("ScoresBot/2.0 (http://www.moyu.life/; hibrick713@gmail.com)")
 games = dota_obj.get_upcoming_and_ongoing_games()
-for i in games: print(i)
-#print(games)
+for i in games:
+  if i.get('team1') == 'Fnatic' or i.get('team2') == 'Fnatic':
+    print(i)
